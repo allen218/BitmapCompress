819e7fe2bd80e92af6dc4625f9c7204fd95751b0
allen <wangcongccnp@126.com>
2017-04-09 18:57:18 +0800

    init commit

---
+++ app/build/intermediates/ndk/debug/obj/local/arm64-v8a/objs/jpegbither/Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/bitherlibjni.o.d
@@ -0,0 +1,32 @@
+/Users/mac/Desktop/NativeImgCompress1/app/build/intermediates/ndk/debug/obj/local/arm64-v8a/objs/jpegbither//Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/bitherlibjni.o: \
+ /Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/bitherlibjni.c \
+ /Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/jpeglib.h \
+ /Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/jconfig.h \
+ /Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/jmorecfg.h \
+ /Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/cdjpeg.h \
+ /Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/jinclude.h \
+ /Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/jpeglib.h \
+ /Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/jerror.h \
+ /Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/cderror.h \
+ /Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/jversion.h \
+ /Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/android/config.h
+
+/Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/jpeglib.h:
+
+/Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/jconfig.h:
+
+/Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/jmorecfg.h:
+
+/Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/cdjpeg.h:
+
+/Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/jinclude.h:
+
+/Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/jpeglib.h:
+
+/Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/jerror.h:
+
+/Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/cderror.h:
+
+/Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/jversion.h:
+
+/Users/mac/Desktop/NativeImgCompress1/app/src/main/jni/libjpeg-turbo/android/config.h:
